d54a260073d4769687a2e9ee3e85df243f6fe68e
abyanphukan434 <72485127+abyanphukan434@users.noreply.github.com>
2020-12-16 11:28:23 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, sum, product;
+    printf("enter two nos");
+    scanf("%d%d", &a, &b);
+    sum = a+b+c;
+    product = a*b*c;
+    printf("output %d", sum, product);
+
+    return 0;
+}
